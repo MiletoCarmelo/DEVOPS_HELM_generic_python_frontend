2ecaaace30bbce56b8622cd3a24cb6510dbb4edb
miletocarmelo <carmelo.mileto@gmail.com>
2024-10-20 14:28:19 +0200

    u

--- documentations/installation_argocd_namespace.sh
+++ documentations/installation_argocd_namespace.sh
@@ -12,8 +12,7 @@ export BASE_HOST=localhost:8090
 echo $BASE_HOST
 
 # forward port 443 to 8090 : 
-kubectl --namespace argocd \
-        port-forward svc/argocd-server 8090:443 &
+kubectl --namespace argocd port-forward svc/argocd-server 8090:443 &
 
 # login into argocd:
 argocd login --insecure --username admin --password $PASS --grpc-web $BASE_HOST
@@ -23,3 +22,16 @@ argocd account update-password --current-password $PASS --new-password Admin123
 
 # INSTALLATION DES METRICS DE CONTROLE USAGE MEMOIRE :
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+# login into argocd:
+argocd login --insecure --username admin --password Admin123 --grpc-web $BASE_HOST
+
+
+
+### SETTUP FOR prod et dev env : 
+# create namespace for dev and prod env :
+kubectl create namespace dev
+kubectl create namespace prod
+
+# create app for dev and prod env :
+argocd app set argocd/trading-strategy-analysis-dev 
